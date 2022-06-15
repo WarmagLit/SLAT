446a310518732dbda9b6110705ffdd5d2c405818
WarmagLit <genzeartem0@gmail.com>
2022-06-15 23:05:02 +0700

    Calendar activity assembled * CalendarEvent data class for the workouts * CalendarEventsAdapter for setting onClick events * MonthViewContainer now contains "Week legend" * Extentions class for all new functionallity of the calendar

--- app/src/main/java/com/tsu/slat/presentation/screens/client_menu/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/tsu/slat/presentation/screens/client_menu/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.tsu.slat.presentation.screens.client_menu.ui.profile
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.tsu.slat.databinding.FragmentProfileBinding
+import com.tsu.slat.presentation.screens.calendar.CalendarActivity
 
 class ProfileFragment : Fragment() {
 
@@ -28,6 +30,10 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.btnCalendar.setOnClickListener {
+            val intent = Intent(requireContext(), CalendarActivity::class.java)
+            startActivity(intent)
+        }
 
 
         return root
